2b405c852e68367fc50b46832f09a3302f92d467
mytsx <mehmet49946@outlook.com>
2022-04-07 10:21:23 +0300

    v1

---
+++ dogruUzerindekiTamSayilar.py
@@ -0,0 +1,18 @@
+p1 = (658000, 4207400)
+p2 = (659000, 4208280)
+
+x1, y1 = p1[0], p1[1]
+x2, y2 = p2[0], p2[1]
+
+m = (y2-y1)/(x2-x1)
+
+print(m)
+
+# y - b = m*(x-a)
+# y = m*(x-a) + b
+# x = ((y-b)/m)+a
+a, b = p1
+for y in range(p1[1],p2[1]+1):
+    x = ((y-b)/m)+a
+    if x - int(x) == 0:
+        print(y, int(x))
