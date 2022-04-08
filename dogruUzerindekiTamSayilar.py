b47671d220b0ed8da36359b28975f343c427af53
mytsx <mehmet49946@outlook.com>
2022-04-09 02:28:30 +0300

    m = 0 güncelleme

--- dogruUzerindekiTamSayilar.py
+++ dogruUzerindekiTamSayilar.py
@@ -1,5 +1,5 @@
-p1 = (658000, 4207400)
-p2 = (659000, 4208280)
+p1 = (657650, 4208500)
+p2 = (659000, 4208500)
 
 x1, y1 = p1[0], p1[1]
 x2, y2 = p2[0], p2[1]
@@ -12,7 +12,16 @@ print(m)
 # y = m*(x-a) + b
 # x = ((y-b)/m)+a
 a, b = p1
-for y in range(p1[1],p2[1]+1):
-    x = ((y-b)/m)+a
-    if x - int(x) == 0:
-        print(y, int(x))
+if p1[1] == p2[1]:
+    y = p1[1]
+    for x in range(p1[0], p2[0]+1):
+        print(x, y)
+elif p1[0] == p2[0]:
+    x = p1[0]
+    for y in range(p1[1],p2[1]+1):
+        print(x, y)
+else:
+    for y in range(p1[1],p2[1]+1):
+        x = ((y-b)/m)+a
+        if x - int(x) == 0:
+            print(y, int(x))
